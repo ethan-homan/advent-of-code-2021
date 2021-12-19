84ae603a5c28e5fde5c5faa1746c10b4001dfe36
ethan-homan <ehoman@better.com>
2021-12-18 21:11:57 -0500

    Comment Day 14

--- day-14/solution.py
+++ day-14/solution.py
@@ -26,12 +26,17 @@ def iterate_n(n, s, char_map):
     for i in range(n):
         new_counts = {i: 0 for i in char_map.keys()}
         for pair in pair_counts:
+            # Split the pair of characters into the first and second
             first, second = pair
+            # Get the new character
             new_char = char_map[pair]
+            # Create two new pairs by appending the new character to the first character and prepending it to the second
             first = first + new_char
             second = new_char + second
+            # Increment the counts for the two new pairs by the count of the original pair that existed
             new_counts[first] += pair_counts[pair]
             new_counts[second] += pair_counts[pair]
+            # Increment the count for the new character
             char_counts[new_char] += pair_counts[pair]
         pair_counts = new_counts
     counts = sorted(char_counts.values())
